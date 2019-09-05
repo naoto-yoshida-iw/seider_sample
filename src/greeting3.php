6585c655ef6ace84ea45785bfd668da0ab800904
Naoto Yoshida <nyoshida@interworks.jp>
2019-09-05 16:27:43 +0900

    Add greeting2.php

---
+++ src/greeting3.php
@@ -0,0 +1,12 @@
+<?php
+namespace greeting3;
+
+class Greeting3
+{
+    public $name;
+    public $word;
+
+    public function say() {
+        return "$this->name くん $this->word\n";
+    }
+}
--- src/main.php
+++ src/main.php
@@ -1,6 +1,7 @@
 <?php
 	require_once("greeting.php");
 require_once("greeting2.php");
+require_once("greeting3.php");
 $rem = new greeting();
 $rem->name = 'レム';
 $rem->word = 'おはよう';
@@ -14,6 +15,11 @@ $g2->word = 'おはよう';
 
 echo $g2->say();
 
+$g3 = new greeting3\Greeting3();
+$g3->name = '炭治郎';
+$g3->word = 'おはよう';
+
+echo $g3->say();
 	if (1) {
 echo "test";
 }
